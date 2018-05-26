ad1fa98f0dbe4739befc4c3b99d5b28c4df42348
Kennan Smith <kennan.smith1@gmail.com>
2018-05-26 13:12:06 -0600

    filled out basic sql; this git add -A came with millions of warnings involving 'LF will be replaced by CRLF' - not sure if it matters, but keep in mind

---
+++ bamazon-schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(150) NOT NULL,
+    department_name VARCHAR(150),
+    price DECIMAL(9,2),
+    stock_quantity INT,
+
+    PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+    VALUES ("Big Ol' TV", "electronics", 250, 10),
+           ("Gaming Laptop", "electronics", 950, 5),
+           ("Old Rug", "furniture", 10, 1),
+           ("Baseball Glove", "sporting equipment", 20, 20),
+           ("Workout Bench", "sporting equipment", 100, 15),
+           ("Single Purpose Kitchen Crap", "cooking", 5, 1000),
+           ("Full-Stack Web Dev Certificate", "education", 12000, 25),
+           ("Calvin & Hobbes Collection", "books", 100, 3),
+           ("Skateboard", "sporting equipment", 75, 25),
+           ("Coffee Maker", "cooking", 50, 100);
+
+SELECT * FROM products;
